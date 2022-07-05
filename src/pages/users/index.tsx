39d4b4ebc0f318927c76750152433529762d9481
Pedro Ceolato <pedroceolato2002@gmail.com>
2022-07-05 19:00:16 -0300

    feat: componente Pagination

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th,
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
@@ -9,109 +10,110 @@ export default function UserList() {
         <Box>
             <Header />
 
-            <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6"> 
+            <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
                 <Sidebar />
 
-            <Box flex="1" borderRadius={8} bg="gray.800" p="8">
-                <Flex mb="8" justify="space-between" align="center">
-                    <Heading size="lg" fontWeight="normal">Usuários</Heading>
+                <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+                    <Flex mb="8" justify="space-between" align="center">
+                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
 
-                    <Button 
-                        as="a" 
-                        size="sm" 
-                        fontSize="sm" 
-                        colorScheme="pink"
-                        leftIcon={<Icon as={RiAddLine} fontSize="20"/>}
+                        <Button
+                            as="a"
+                            size="sm"
+                            fontSize="sm"
+                            colorScheme="pink"
+                            leftIcon={<Icon as={RiAddLine} fontSize="20" />}
                         >
-                        Criar novo
-                    </Button>
-                </Flex>
+                            Criar novo
+                        </Button>
+                    </Flex>
 
-                <Table colorScheme="whiteAlpha">
-                    <Thead>
-                        <Tr>
-                            <Th px="6" color="gray.300" width="8">
-                                <Checkbox colorScheme="pink" />
-                            </Th>
-                            <Th>Usuário</Th>
-                            <Th>Data de cadastro</Th>
-                            <Th w="8"></Th> 
-                        </Tr>
-                    </Thead>
-                    <Tbody>
-                        <Tr>
-                            <Td px="6">
-                            <Checkbox colorScheme="pink" />
-                            </Td>
-                            <Td>
-                                <Box>
-                                    <Text fontWeight="bold">Pedro Ceolato</Text>
-                                    <Text fontSize="sm" color="gray.300">pedroceolato2002@gmail.com</Text>
-                                </Box>  
-                            </Td>
-                            <Td>04 de Abril, 2021</Td>
-                            <Td>
-                                <Button 
-                                    as="a" 
-                                    size="sm" 
-                                    fontSize="sm" 
-                                    colorScheme="purple"
-                                    leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
-                                >
-                                    Editar
-                                </Button>
-                            </Td>
-                        </Tr>
-                        <Tr>
-                            <Td px="6">
-                            <Checkbox colorScheme="pink" />
-                            </Td>
-                            <Td>
-                                <Box>
-                                    <Text fontWeight="bold">Pedro Ceolato</Text>
-                                    <Text fontSize="sm" color="gray.300">pedroceolato2002@gmail.com</Text>
-                                </Box>  
-                            </Td>
-                            <Td>04 de Abril, 2021</Td>
-                            <Td>
-                                <Button 
-                                    as="a" 
-                                    size="sm" 
-                                    fontSize="sm" 
-                                    colorScheme="purple"
-                                    leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
-                                >
-                                    Editar
-                                </Button>
-                            </Td>
-                        </Tr>
-                        <Tr>
-                            <Td px="6">
-                            <Checkbox colorScheme="pink" />
-                            </Td>
-                            <Td>
-                                <Box>
-                                    <Text fontWeight="bold">Pedro Ceolato</Text>
-                                    <Text fontSize="sm" color="gray.300">pedroceolato2002@gmail.com</Text>
-                                </Box>  
-                            </Td>
-                            <Td>04 de Abril, 2021</Td>
-                            <Td>
-                                <Button 
-                                    as="a" 
-                                    size="sm" 
-                                    fontSize="sm" 
-                                    colorScheme="purple"
-                                    leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
-                                >
-                                    Editar
-                                </Button>
-                            </Td>
-                        </Tr>
-                    </Tbody>
-                </Table>
-            </Box>
+                    <Table colorScheme="whiteAlpha">
+                        <Thead>
+                            <Tr>
+                                <Th px="6" color="gray.300" width="8">
+                                    <Checkbox colorScheme="pink" />
+                                </Th>
+                                <Th>Usuário</Th>
+                                <Th>Data de cadastro</Th>
+                                <Th w="8"></Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            <Tr>
+                                <Td px="6">
+                                    <Checkbox colorScheme="pink" />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight="bold">Pedro Ceolato</Text>
+                                        <Text fontSize="sm" color="gray.300">pedroceolato2002@gmail.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td>04 de Abril, 2021</Td>
+                                <Td>
+                                    <Button
+                                        as="a"
+                                        size="sm"
+                                        fontSize="sm"
+                                        colorScheme="purple"
+                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                                    >
+                                        Editar
+                                    </Button>
+                                </Td>
+                            </Tr>
+                            <Tr>
+                                <Td px="6">
+                                    <Checkbox colorScheme="pink" />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight="bold">Pedro Ceolato</Text>
+                                        <Text fontSize="sm" color="gray.300">pedroceolato2002@gmail.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td>04 de Abril, 2021</Td>
+                                <Td>
+                                    <Button
+                                        as="a"
+                                        size="sm"
+                                        fontSize="sm"
+                                        colorScheme="purple"
+                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                                    >
+                                        Editar
+                                    </Button>
+                                </Td>
+                            </Tr>
+                            <Tr>
+                                <Td px="6">
+                                    <Checkbox colorScheme="pink" />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight="bold">Pedro Ceolato</Text>
+                                        <Text fontSize="sm" color="gray.300">pedroceolato2002@gmail.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td>04 de Abril, 2021</Td>
+                                <Td>
+                                    <Button
+                                        as="a"
+                                        size="sm"
+                                        fontSize="sm"
+                                        colorScheme="purple"
+                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                                    >
+                                        Editar
+                                    </Button>
+                                </Td>
+                            </Tr>
+                        </Tbody>
+                    </Table>
 
+                    <Pagination />
+                </Box>
             </Flex>
         </Box>
     );
