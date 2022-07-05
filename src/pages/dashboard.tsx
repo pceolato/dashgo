3a4fff577a22c9070665a1485fa964ec0f545508
Pedro Ceolato <pedroceolato2002@gmail.com>
2022-07-05 16:39:58 -0300

    feat: componente SideBar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,36 @@
+import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
 import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
 
 export default function Dashboard() {
     return (
-        <Header />
+    <Flex direction="column" h="100vh">
+            <Header />
+            
+             <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6"> 
+                <Sidebar />
+
+                <SimpleGrid flex="1" gap="4" minChildWidth="320px" alignItems="flex-start"> 
+                    <Box
+                        p="8"
+                        bg="gray.800"
+                        borderRadius={8}
+                        h="100%"
+                        // pb="4"
+                    >
+                        <Text fontSize="lg" mb="4">Inscritos da semana</Text>
+                    </Box>
+                    <Box
+                        p="8"
+                        bg="gray.800"
+                        borderRadius={8}
+                        h="100%"
+                        // pb="4"
+                    >
+                        <Text fontSize="lg" mb="4">Taxa de Abertura</Text>
+                    </Box>
+                </SimpleGrid>
+             </Flex>
+        </Flex>
     )
 }
