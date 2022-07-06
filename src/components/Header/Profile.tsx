a682ce1411827a93ada09100fe1b0e3294baf683
Pedro Ceolato <pedroceolato2002@gmail.com>
2022-07-06 17:17:09 -0300

    refactor: Header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData: boolean;
+}
+
+export function Profile({showProfileData = true}: ProfileProps) {
     return (
         <Flex align="center">
-            <Box mr="4" textAlign="right">
+            { showProfileData && (
+                <Box mr="4" textAlign="right">
                 <Text>Pedro Ceolato</Text>
                 <Text color="gray.300" fontSize="small">
                     pedroceolato2002@gmail.com
                 </Text>
             </Box>
+            )}
 
             <Avatar size="md" name="Pedro Ceolato" src="https://github.com/pceolato.png" />
         </Flex>
