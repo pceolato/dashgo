53ab361ec30a53ce66a9367dc512e9fc7356d06c
Pedro Ceolato <pedroceolato2002@gmail.com>
2022-07-13 18:57:10 -0300

    feat: prefetch de dados

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,15 @@ import { AppProps } from 'next/app'
 import { ReactQueryDevtools } from 'react-query/devtools'
 import { theme } from '../styles/theme'
 
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { QueryClientProvider } from 'react-query'
 import { ChakraProvider } from '@chakra-ui/react'
 import { makeServer } from '../services/mirage'
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext'
+import { queryClient } from '../services/queryClient'
 
 if (process.env.NODE_ENV === 'development') {
   makeServer();
 }
-
-const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
